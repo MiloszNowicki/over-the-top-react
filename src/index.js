0fe4bb6e9c0a4f43ffaf6fcda8a136a80766089f
Milosz Nowicki <milosz.nowicki@edpauto.com>
2018-05-21 07:48:41 +0200

    old react package

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Greeter extends React.Component {
+    render() {
+        return (
+            <div>Just look at this positive message!</div>
+        );
+    }
+}
+export default Greeter;
